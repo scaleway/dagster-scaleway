798262ecfa595dd9f800000f37397abde4965d56
Andy Méry <andy@mery.cloud>
2024-01-14 20:38:41 +0100

    feat: add web url output

--- terraform/README.md
+++ terraform/README.md
@@ -1,2 +1,3 @@
 # Scaleway Dagster Terraform Module
 
+A Terraform module to deploy Dagster on Serverless Containers and Jobs.
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,3 +8,17 @@ output "rdb_admin_password" {
   value       = local.rdb_admin_password
 }
 
+output "rdb_password" {
+  description = "The password to be used for the dagster user of the relational database."
+  sensitive   = true
+  value       = local.rdb_password
+}
+
+################################################################################
+# Serverless Containers
+################################################################################
+
+output "dagster_webserver_url" {
+  description = "The URL of the Dagster webserver."
+  value       = "https://${scaleway_serverless_container.webserver.domain_name}"
+}
